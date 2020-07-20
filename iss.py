420297df975b2a86c45b8d0390d4d36c7c7d67e0
kbrooks87 <kbrooks10011987@gmail.com>
2020-07-20 09:58:19 -0400

    finished

--- iss.py
+++ iss.py
@@ -1,70 +1,109 @@
 #!/usr/bin/env python
 
-__author__ = 'Kelly Brooks with help from Kathryn'
+__author__ = 'Kelly Brooks with help from Kathryn, Chris and Jackson video from after hours work '
 
 
 import turtle
 import time
 import requests
 
-def people_in_space():
-    # Retrieves list of people in space and their craft
-    retrieve = requests.get('http://api.open-notify.org/astros.json')
-    space = retrieve.json()
+iss_icon = 'iss.gif'
+world_map = 'map.gif'
+base_url = 'http://api.open-notify.org'
 
-    print("Total Astronauts in Space: ", space['number'])
+def get_astronaut_info():
 
-    for person in space["people"]:
-        print("Astronaut: {}, Spacecraft: {}".format(
-            person["name"],
-            person["craft"]
-        ))
-    
-def iss_position():
-    # Retrieves location of ISS
-    retrieve = requests.get('http://api.open-notify.org/iss-now.json')
-    location_of_station = retrieve.json()
-    return location_of_station
+    """
+    Get a list of the astronatuts currently in space.
+    Include their names, the spacecraft that they are
+    currently on board, and the total number of
+    astronatuts in space.
+    """
+    r = requests.get(base_url + '/astros.json')
+    r.raise_for_status()
+    return r.json()['people']
 
 
-def time_in_indy():
-    # Retrieves flight time of ISS over specific Lat/Long Coordinates
-    retrieve = requests.get(
-        'http://api.open-notify.org/iss-pass.json?lat=39.768&lon=-86.158')
-    flight_time = retrieve.json()
-    return time.ctime(flight_time["response"][1]["risetime"])
+def locate_iss_spacestation():
+    """
+    Get the current geographic coordinates (lat/lon) of the space station, along with a timestamp.
+    Get ISS position (lon, lat) as a float tuple
+    """
+    r = requests.get(base_url + '/iss-now.json')
+
+    # handles response from server just incase you get anything other than a 200 response
+    r.raise_for_status()
+    position = r.json()['iss_position']
+    lat = float(position['latitude'])
+    lon = float(position['longitude'])
+    return lat, lon
 
 
-def space_turtle():
-    # Screen displaying the World Map and ISS 
-    display = turtle.Screen()
-    display.setup(600, 400)
-    display.bgpic("./map.gif")
-    display.reset()
-    display.setworldcoordinates(-180, -90, 180, 90)
-    display.addshape("iss.gif")
-    display.title("ISS Position Locator")
+def map_iss(lat, lon):
+    """
+    Draw a world map and place ISS icon at lat, lon
+    """
+    screen = turtle.Screen()
+    screen.setup(720, 360)
+    screen.bgpic(world_map)
+    screen.setworldcoordinates(-180, -90, 180, 90)
 
-    # Creates turtle
-    turtle.shape("iss.gif")
-    turtle.penup()
-    turtle.goto(-65, 70)
-    turtle.color("green")
-    turtle.write(time_in_indy(), font=("Arial", 15, "bold"))
-    turtle.goto(float(-86.158), float(39.768))
-    turtle.dot(5, 'red')
+    screen.register_shape(iss_icon)
+    iss = turtle.Turtle()
+    iss.shape(iss_icon)
+    iss.setheading(90)
+    iss.penup()
+    iss.goto(lon, lat)  # note the lon and lat are in reverse order here
+    return screen
 
-    while True:
-        iss = iss_position()
-        position = iss["iss_position"]
-        turtle.goto(float(position["longitude"]), float(position["latitude"]))
-        time.sleep(10)
+
+def compute_rise_time(lat, lon):
+    """
+    Return the next horizon rise-time of ISS for specific lat/lon
+    """
+    params = {'lat': lat, 'lon': lon}
+    r = requests.get(base_url + '/iss-pass.json', params=params)
+    r.raise_for_status()
+
+    passover_time = r.json()['response'][1]['risetime']
+    return time.ctime(passover_time)
 
 
 def main():
-    people_in_space()
-    time_in_indy()
-    space_turtle()
+    # Part A - get the astronauts and their crafts
+    astro_dict = get_astronaut_info()
+    print('Current people in space: {}'.format(len(astro_dict)))
+    for a in astro_dict:
+        print(' - {} in {}'.format(a['name'], a['craft']))
+
+    # Part B - get current position of ISS
+    lat, lon = locate_iss_spacestation()
+    print('Current ISS coordinates: lat={:.02f} lon={:.02f}'.format(lat, lon))
+
+    # Part C - Render ISS on world map
+    screen = None
+    try:
+        # Attempts to show turtle
+        screen = map_iss(lat, lon)
+
+        # Part D - Compute the next pass-over time for Indianapolis, IN
+        indy_lat = 39.768403
+        indy_lon = -86.158068
+        location = turtle.Turtle()
+        location.penup()
+        location.color('yellow')
+        location.goto(indy_lon, indy_lat)
+        location.dot(5)
+        location.hideturtle()
+        next_pass = compute_rise_time(indy_lat, indy_lon)
+        location.write(next_pass, align='center', font=('Arial', 12, 'normal'))
+    except RuntimeError as e:
+        print('ERROR: problem loading graphics: ' + str(e))
+
+    # leave screen open until user clicks on it
+    if screen is not None:
+        print('Click on screen to exit...')
+        screen.exitonclick()
 
 
 if __name__ == '__main__':
